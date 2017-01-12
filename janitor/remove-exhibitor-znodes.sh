b47b4b60f67055f816d05dc6147ee508ef1eb02c
Vishnu Mohan <vishnu@mesosphere.com>
2017-01-12 08:07:30 -0500

    Use the key instead of the title to remove the znode

--- janitor/remove-exhibitor-znodes.sh
+++ janitor/remove-exhibitor-znodes.sh
@@ -10,14 +10,15 @@ exhibitor_url=$(dcos config show core.dcos_url)/exhibitor/
 token=$(dcos config show core.dcos_acs_token)
 
 jq -er '. | keys[]' "${exhibitor_znodes_json}" | while read -r key ; do
-        znode=$(jq -er ".[$key].title" "${exhibitor_znodes_json}")
+        znode_title=$(jq -er ".[$key].title" "${exhibitor_znodes_json}")
+        znode_key=$(jq -er ".[$key].key" "${exhibitor_znodes_json}")
 
-        echo "Removing Znode: ${znode}"
+        echo "Removing znode ${znode_title}: ${znode_key}"
         python janitor.py \
             -v \
             -m "${master_url}" \
             -n "${marathon_url}" \
             -e "${exhibitor_url}" \
-            -z "${znode}" \
+            -z "${znode_key}" \
             --auth_token="${token}"
 done
