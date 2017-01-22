1a99c34b9cf8bd2d1701dbd5c8e09f4c5b2a686e
Vishnu Mohan <vishnu@mesosphere.com>
2017-01-21 19:06:36 -0500

    Added cleanup helper

---
+++ marathon/nginx/remove-nginx-apps-from-marathon.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+
+for i in $(dcos marathon app list --json | jq -er '.[] | select(.id | match("nginx")) | .id')
+do 
+    dcos marathon app remove "${i}"
+done
