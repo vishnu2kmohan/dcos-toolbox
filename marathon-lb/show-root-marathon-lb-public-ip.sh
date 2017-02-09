ae6811186ddb8749e856d07e34c2dc8c1bd8d5bd
Vishnu Mohan <vishnu@mesosphere.com>
2017-02-08 20:27:46 -0500

    Quiet SSH Log Level

--- marathon-lb/show-root-marathon-lb-public-ip.sh
+++ marathon-lb/show-root-marathon-lb-public-ip.sh
@@ -3,9 +3,10 @@
 set -o errexit -o nounset -o pipefail
 
 dcos node ssh \
+    --option LogLevel=quiet \
     --option StrictHostKeyChecking=no \
     --option UserKnownHostsFile=/dev/null \
     --option BatchMode=yes \
     --option PasswordAuthentication=no \
     --master-proxy --leader \
-    "ssh -A -t -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes -o PasswordAuthentication=no $(dcos marathon app show /dcos/root/marathon-lb | jq -r .tasks[].host) curl -sf http://ipecho.net/plain"
+    "ssh -A -t -o LogLevel=quiet -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes -o PasswordAuthentication=no $(dcos marathon app show /dcos/root/marathon-lb | jq -r .tasks[].host) curl -sf http://ipecho.net/plain"
--- spark/spark-docker-pull.sh
+++ spark/spark-docker-pull.sh
@@ -2,14 +2,14 @@
 
 set -o errexit -o nounset -o pipefail
 
-for i in $(dcos node --json | jq -er .[].id) ; do (dcos node ssh --option StrictHostKeyChecking=no --option UserKnownHostsFile=/dev/null --option BatchMode=yes --option PasswordAuthentication=no --master-proxy --mesos-id="$i" "'docker pull mesosphere/spark:1.0.6-2.0.2-hadoop-2.6'" &) 2>/dev/null & done
+for i in $(dcos node --json | jq -er .[].id) ; do (dcos node ssh --option LogLevel=quiet --option StrictHostKeyChecking=no --option UserKnownHostsFile=/dev/null --option BatchMode=yes --option PasswordAuthentication=no --master-proxy --mesos-id="$i" "'docker pull mesosphere/spark:1.0.6-2.0.2-hadoop-2.6'" &) 2>/dev/null & done
 wait
 
-for i in $(dcos node --json | jq -er .[].id) ; do (dcos node ssh --option StrictHostKeyChecking=no --option UserKnownHostsFile=/dev/null --option BatchMode=yes --option PasswordAuthentication=no --master-proxy --mesos-id="$i" "'docker pull mesosphere/spark:1.0.5-1.6.3-hadoop-2.6'" &) 2>/dev/null & done
+for i in $(dcos node --json | jq -er .[].id) ; do (dcos node ssh --option LogLevel=quiet --option StrictHostKeyChecking=no --option UserKnownHostsFile=/dev/null --option BatchMode=yes --option PasswordAuthentication=no --master-proxy --mesos-id="$i" "'docker pull mesosphere/spark:1.0.5-1.6.3-hadoop-2.6'" &) 2>/dev/null & done
 wait
 
-#for i in $(dcos node --json | jq -er .[].id) ; do (dcos node ssh --option StrictHostKeyChecking=no --option UserKnownHostsFile=/dev/null --option BatchMode=yes --option PasswordAuthentication=no --master-proxy --mesos-id="$i" "'docker pull mesosphere/spark:1.5.0-multi-roles-v2-bin-2.4.0'" &) 2>/dev/null & done
+#for i in $(dcos node --json | jq -er .[].id) ; do (dcos node ssh --option LogLevel=quiet --option StrictHostKeyChecking=no --option UserKnownHostsFile=/dev/null --option BatchMode=yes --option PasswordAuthentication=no --master-proxy --mesos-id="$i" "'docker pull mesosphere/spark:1.5.0-multi-roles-v2-bin-2.4.0'" &) 2>/dev/null & done
 #
 
-for i in $(dcos node --json | jq -er .[].id) ; do (dcos node ssh --option StrictHostKeyChecking=no --option UserKnownHostsFile=/dev/null --option BatchMode=yes --option PasswordAuthentication=no --master-proxy --mesos-id="$i" "'docker images'" &) 2>/dev/null & done
+for i in $(dcos node --json | jq -er .[].id) ; do (dcos node ssh --option LogLevel=quiet --option StrictHostKeyChecking=no --option UserKnownHostsFile=/dev/null --option BatchMode=yes --option PasswordAuthentication=no --master-proxy --mesos-id="$i" "'docker images'" &) 2>/dev/null & done
 wait
