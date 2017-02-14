3d1df23dd476fcf537ca22499efbca7d88a3e8b9
Vishnu Mohan <vishnu@mesosphere.com>
2017-02-14 10:45:34 -0500

    Handle multiple Marathon-LB instances

--- marathon-lb/show-root-marathon-lb-public-ip.sh
+++ marathon-lb/show-root-marathon-lb-public-ip.sh
@@ -2,11 +2,22 @@
 
 set -o errexit -o nounset -o pipefail
 
-dcos node ssh \
-    --option LogLevel=quiet \
-    --option StrictHostKeyChecking=no \
-    --option UserKnownHostsFile=/dev/null \
-    --option BatchMode=yes \
-    --option PasswordAuthentication=no \
-    --master-proxy --leader \
-    "ssh -A -t -o LogLevel=quiet -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes -o PasswordAuthentication=no $(dcos marathon app show /dcos/root/marathon-lb | jq -r .tasks[].host) curl -sf http://ipecho.net/plain"
+for agent in $(dcos marathon app show /dcos/root/marathon-lb | \
+               jq -r .tasks[].host)
+do
+    echo $(dcos node ssh \
+        --option LogLevel=quiet \
+        --option StrictHostKeyChecking=no \
+        --option UserKnownHostsFile=/dev/null \
+        --option BatchMode=yes \
+        --option PasswordAuthentication=no \
+        --master-proxy --leader \
+        "ssh -A -t \
+            -o LogLevel=quiet \
+            -o StrictHostKeyChecking=no \
+            -o UserKnownHostsFile=/dev/null \
+            -o BatchMode=yes \
+            -o PasswordAuthentication=no \
+            ${agent} \
+            curl -sSL http://ipecho.net/plain" 2>/dev/null)
+done
