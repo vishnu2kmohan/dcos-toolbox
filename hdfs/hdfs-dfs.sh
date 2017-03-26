f318c1934c0177b100ccc814af64b00021ac3d71
Vishnu Mohan <vishnu@mesosphere.com>
2017-03-26 17:55:25 -0400

    Modify to run from Metronome/Chronos

--- hdfs/hdfs-dfs.sh
+++ hdfs/hdfs-dfs.sh
@@ -2,10 +2,14 @@
 
 set -o errexit -o nounset -o pipefail
 
-docker run -it mesosphere/hdfs-client bash
-wget http://api.hdfs.marathon.l4lb.thisdcos.directory/v1/endpoints/hdfs-site.xml
-wget http://api.hdfs.marathon.l4lb.thisdcos.directory/v1/endpoints/core-site.xml
-cp core-site.xml hadoop-2.6.4/etc/hadoop
-cp hdfs-site.xml hadoop-2.6.4/etc/hadoop
+export HADOOP_CONF_DIR="${MESOS_SANDBOX}"
 export PATH=$PATH:/hadoop-2.6.4/bin
+
+#cd "${MESOS_SANDBOX}"
+#docker run -it mesosphere/hdfs-client bash
+#wget http://api.hdfs.marathon.l4lb.thisdcos.directory/v1/endpoints/hdfs-site.xml
+#wget http://api.hdfs.marathon.l4lb.thisdcos.directory/v1/endpoints/core-site.xml
+#cp core-site.xml hadoop-2.6.4/etc/hadoop
+#cp hdfs-site.xml hadoop-2.6.4/etc/hadoop
+
 hdfs dfs -ls -R /
