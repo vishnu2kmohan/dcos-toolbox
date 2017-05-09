fcc42c3fa7ebf309406fbfb5816548a18b49d1d5
Vishnu Mohan <vishnu@mesosphere.com>
2017-05-09 14:38:48 -0400

    Add zk.set() logic

--- bouncer/kazoo/load-bouncer-datastore-data.py
+++ bouncer/kazoo/load-bouncer-datastore-data.py
@@ -9,11 +9,15 @@ DCOS_ZK_AUTH_DATA=[(DCOS_ZK_AUTH_TYPE, DCOS_ZK_CREDS)]
 DCOS_BOUNCER_LOCK_PATH='/bouncer/datastore/locking'
 DCOS_BOUNCER_DATA_PATH='/bouncer/datastore/data.json'
 
+DCOS_BOUNCER_DATA_FILE='bouncer-datastore-data.json'
+
 zk = KazooClient(hosts=DCOS_ZK_HOSTS, auth_data=DCOS_ZK_AUTH_DATA)
 zk.start()
 if zk.exists(DCOS_BOUNCER_DATA_PATH):
-    with zk.Lock(DCOS_BOUNCER_LOCK_PATH):
-        data, stat = zk.get(DCOS_BOUNCER_DATA_PATH)
+    with open(DCOS_BOUNCER_DATA_FILE) as f:
+        with zk.Lock(DCOS_BOUNCER_LOCK_PATH):
+            zk.set(DCOS_BOUNCER_DATA_PATH, f.read().encode())
+            data, stat = zk.get(DCOS_BOUNCER_DATA_PATH)
     print(data.decode())
     print(stat)
 zk.stop()
